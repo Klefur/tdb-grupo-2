2a7a287eadce6f912ab70e4ea61671a8b990e37d
Skyrdow <lucas.mesias@usach.cl>
2023-10-06 21:35:46 -0300

    feat: tailwind

--- lab1/front-lab-1/src/App.vue
+++ lab1/front-lab-1/src/App.vue
@@ -1,8 +1,6 @@
 <template>
-	<nav>
-		<router-link to="/">Home</router-link> |
-		<router-link to="/emergencias">About</router-link>
-	</nav>
+	<router-link to="/">Home</router-link> |
+	<router-link to="/emergencias">About</router-link>
 	<router-view />
 </template>
 
@@ -13,18 +11,5 @@
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-}
-
-nav {
-	padding: 30px;
-}
-
-nav a {
-	font-weight: bold;
-	color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-	color: #42b983;
 }
 </style>
--- lab1/front-lab-1/src/views/ListaEmergencias.vue
+++ lab1/front-lab-1/src/views/ListaEmergencias.vue
@@ -2,14 +2,36 @@
 	<div>
 		<h1>This is an about page</h1>
 	</div>
-	<h1>Lista de Emergencias:</h1>
 	<h2>Total de Emergencias activas: {{ tareasActivasTotal }}</h2>
-	<div v-for="(item, index) in listaEmergencias" :key="index">
-		{{ item }}
+	<div class="flex flex-col items-center">
+		<h1>Lista de Emergencias:</h1>
+
+		<div
+			class="flex flex-col items-center bg-gray-400 border w-fit rounded-xl"
+		>
+			<div
+				v-for="(item, index) in listaEmergencias"
+				class="flex flex-row bg-slate-400 border border-black"
+				:key="index"
+			>
+				<p>
+					{{ item }}
+				</p>
+				<button
+					@click="FuncionalidadBoton(index)"
+					class="bg-green-400 p-2"
+				>
+					activar
+				</button>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script setup>
+function FuncionalidadBoton(x) {
+	console.log(x);
+}
 function GetDatos() {
 	const datos = {
 		lista: [1, 2, 3, 4, 5],
