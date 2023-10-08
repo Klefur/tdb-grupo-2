089c8e4f51e2dcfc9a1859d02e7c5a8d5c2ac5c3
diegovaldes23 <diego.valdes.f@usach.cl>
2023-10-08 20:22:53 -0300

    Auto stash before merge of "main" and "origin/main"
    task imp y task_ability imp

--- lab1/Backend/voluntariado/src/main/java/com/example/voluntariado/repositoriesImp/TaskImp.java
+++ lab1/Backend/voluntariado/src/main/java/com/example/voluntariado/repositoriesImp/TaskImp.java
@@ -2,42 +2,103 @@ package com.example.voluntariado.repositoriesImp;
 
 import com.example.voluntariado.models.Task;
 import com.example.voluntariado.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 
 import java.util.List;
 
+@Repository
 public class TaskImp implements TaskRepository {
+    @Autowired
+    private Sql2o sql2o;
+
     @Override
     public List<Task> getAllTasks() {
-        return null;
+        try(Connection conn = sql2o.open()){
+            return conn.createQuery("SELECT * FROM \"Task\"")
+                    .executeAndFetch(Task.class);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public int countTasksByEmergencyId(Integer id) {
+        // Implementa tu lógica aquí
         return 0;
     }
 
     @Override
-    public Task getTaskById(Integer id) {
-        return null;
+    public List<Task> getTaskById(Integer id) {
+        try(Connection conn = sql2o.open()){
+            return conn.createQuery("SELECT * FROM \"Task\" WHERE id_task = :id")
+                    .addParameter("id_task", id)
+                    .executeAndFetch(Task.class);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public Task createTask(Task task) {
-        return null;
+        try(Connection conn = sql2o.open()){
+            conn.createQuery("INSERT INTO \"Task\" (name, description, state, id_institution)"+
+                            "values (:taskName, :taskDescription, :taskState, :taskInstitution)")
+                    .addParameter("taskName", task.getName())
+                    .addParameter("taskDescription", task.getDescription())
+                    .addParameter("taskState", task.getState())
+                    .addParameter("taskInstitution", task.getId_institution())
+                    .executeUpdate().getKey();
+            return task;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public Task editTask(Task task) {
-        return null;
+        try(Connection conn = sql2o.open()){
+            conn.createQuery("UPDATE \"Task\" SET name = :name, description = :description, state = :state, id_institution = :id_institution WHERE id_task = :id_task")
+                    .addParameter("id_task", task.getId_task())
+                    .addParameter("name", task.getName())
+                    .addParameter("description", task.getDescription())
+                    .addParameter("state", task.getState())
+                    .addParameter("id_institution", task.getId_institution())
+                    .executeUpdate();
+            return task;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public boolean deleteTaskById(Integer id) {
-        return false;
+        int deletedTask;
+        try(Connection conn = sql2o.open()){
+            deletedTask = conn.createQuery("DELETE FROM \"Task\" WHERE id_task = :id")
+                    .addParameter("id_task", id)
+                    .executeUpdate().getResult();
+        }
+        return deletedTask == 1;
     }
 
     @Override
     public boolean deleteAllTasks() {
-        return false;
+        try(Connection conn = sql2o.open()){
+            conn.createQuery("TRUNCATE \"Task\" CASCADE")
+                    .executeUpdate().getResult();
+            return true;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 }
+
--- lab1/Backend/voluntariado/src/main/java/com/example/voluntariado/repositoriesImp/Task_AbilityImp.java
+++ lab1/Backend/voluntariado/src/main/java/com/example/voluntariado/repositoriesImp/Task_AbilityImp.java
@@ -2,37 +2,92 @@ package com.example.voluntariado.repositoriesImp;
 
 import com.example.voluntariado.models.Task_Ability;
 import com.example.voluntariado.repositories.Task_AbilityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import java.util.List;
 
+@Repository
 public class Task_AbilityImp implements Task_AbilityRepository {
+    @Autowired
+    private Sql2o sql2o;
+
     @Override
     public List<Task_Ability> getAllTasksAbilities() {
-        return null;
+        try(Connection conn = sql2o.open()){
+            return conn.createQuery("SELECT * FROM \"Task_Ability\"")
+                    .executeAndFetch(Task_Ability.class);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
-    public Task_Ability getTaskAbilityById(Integer id) {
-        return null;
+    public List<Task_Ability> getTaskAbilityById(Integer id) {
+        try(Connection conn = sql2o.open()){
+            return conn.createQuery("SELECT * FROM \"Task_Ability\" WHERE id_task_ability = :id")
+                    .addParameter("id_task_ability", id)
+                    .executeAndFetch(Task_Ability.class);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public Task_Ability createTaskAbility(Task_Ability taskAbility) {
-        return null;
+        try(Connection conn = sql2o.open()){
+            conn.createQuery("INSERT INTO \"Task_Ability\" (id_task, id_ability)"+
+                            "values (:taskID, :abilityID)")
+                    .addParameter("taskID", taskAbility.getId_task())
+                    .addParameter("abilityID", taskAbility.getId_ability())
+                    .executeUpdate().getKey();
+            return taskAbility;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public Task_Ability editTaskAbility(Task_Ability taskAbility) {
-        return null;
+        try(Connection conn = sql2o.open()){
+            conn.createQuery("UPDATE \"Task_Ability\" SET id_task = :taskID, id_ability = :abilityID WHERE id_task_ability = :id_task_ability")
+                    .addParameter("id_task_ability", taskAbility.getId_task_ability())
+                    .addParameter("taskID", taskAbility.getId_task())
+                    .addParameter("abilityID", taskAbility.getId_ability())
+                    .executeUpdate();
+            return taskAbility;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public boolean deleteTaskAbilityById(Integer id) {
-        return false;
+        int deletedTaskAbility;
+        try(Connection conn = sql2o.open()){
+            deletedTaskAbility = conn.createQuery("DELETE FROM \"Task_Ability\" WHERE id_task_ability = :id")
+                    .addParameter("id_task_ability", id)
+                    .executeUpdate().getResult();
+        }
+        return deletedTaskAbility == 1;
     }
 
     @Override
     public boolean deleteAllTasksAbilities() {
-        return false;
+        try(Connection conn = sql2o.open()){
+            conn.createQuery("TRUNCATE \"Task_Ability\" CASCADE")
+                    .executeUpdate().getResult();
+            return true;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 }
+
