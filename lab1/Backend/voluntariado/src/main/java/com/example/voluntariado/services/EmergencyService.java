929a0852587224c81a3a6ba0e36ffc584963b885
valenpy22 <valentina.campos.o@usach.cl>
2023-10-08 23:14:14 -0300

    feat: adding EmergencyService

--- lab1/Backend/voluntariado/src/main/java/com/example/voluntariado/services/EmergencyService.java
+++ lab1/Backend/voluntariado/src/main/java/com/example/voluntariado/services/EmergencyService.java
@@ -1,10 +1,46 @@
 package com.example.voluntariado.services;
 
+import com.example.voluntariado.models.Emergency;
+import com.example.voluntariado.repositories.EmergencyRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
 public class EmergencyService {
+    private final EmergencyRepository emergencyRepository;
+
+    EmergencyService(EmergencyRepository emergencyRepository){
+        this.emergencyRepository = emergencyRepository;
+    }
+
+    @GetMapping("/emergencies")
+    public List<Emergency> getAllEmergencies(){
+        return emergencyRepository.getAllEmergencies();
+    }
+
+    @GetMapping("/emergencies/{id_emergency}")
+    public List<Emergency> getEmergencyById(@RequestParam("id_emergency") Integer id_emergency){
+        return emergencyRepository.getEmergencyById(id_emergency);
+    }
+
+    @PostMapping("/emergencies")
+    @ResponseBody
+    public Emergency createEmergency(@RequestBody Emergency emergency){
+        return emergencyRepository.createEmergency(emergency);
+    }
+
+    @PutMapping("/emergencies/{id_emergency}")
+    @ResponseBody
+    public String editEmergency(@PathVariable("id_emergency") Integer id_emergency, @RequestBody Emergency emergency){
+        emergency.setId_emergency(id_emergency);
+        boolean result = emergencyRepository.editEmergency(emergency);
+        if(result){
+            return "Edited emergency";
+        }else{
+            return "Emergency not found";
+        }
+    }
 }
